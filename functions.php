3d4491b44921b41d010cf18917c7e37cc0ccfbfe
ks <zejafo@gmail.com>
2023-11-24 00:52:07 +0400

    add wp files; fix wordpress map

---
+++ functions.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Child functions and definitions.
+ */
+add_filter( 'kava-theme/assets-depends/styles', 'kava_child_styles_depends' );
+
+/**
+ * Enqueue styles.
+ */
+function kava_child_styles_depends( $deps ) {
+
+	$parent_handle = 'kava-parent-theme-style';
+
+	wp_register_style(
+		$parent_handle,
+		get_template_directory_uri() . '/style.css',
+		array(),
+		kava_theme()->version()
+	);
+
+	$deps[] = $parent_handle;
+
+	return $deps;
+}
+
+/**
+ * Disable magic button for your clients
+ *
+ * Un-comment next line to disble magic button output for you clients.
+ */
+//add_action( 'jet-theme-core/register-config', 'kava_child_disable_magic_button' );
+
+function kava_child_disable_magic_button( $config_manager ) {
+	$config_manager->register_config( array(
+		'library_button' => false,
+	) );
+}
+
+/**
+ * Disable unnecessary theme modules
+ *
+ * Un-comment next line and return unnecessary modules from returning modules array.
+ */
+//add_filter( 'kava-theme/allowed-modules', 'kava_child_allowed_modules' );
+
+function kava_child_allowed_modules( $modules ) {
+
+	return array(
+		'blog-layouts'    => array(),
+		'crocoblock'      => array(),
+		'woo'             => array(
+			'woo-breadcrumbs' => array(),
+			'woo-page-title'  => array(),
+		),
+	);
+
+}
+
+/**
+ * Registering a new structure
+ *
+ * To change structure and apropriate documnet type parameters go to
+ * structures/archive.php and document-types/archive.php
+ *
+ * To print apropriate location add next code where you need it:
+ * if ( function_exists( 'jet_theme_core' ) ) {
+ *     jet_theme_core()->locations->do_location( 'kava_child_archive' );
+ * }
+ * Where 'kava_child_archive' - apropritate location name (from example).
+ *
+ * Un-comment next line to register new structure.
+ */
+//add_action( 'jet-theme-core/structures/register', 'kava_child_structures' );
+
+function kava_child_structures( $structures_manager ) {
+
+	require get_theme_file_path( 'structures/archive.php' );
+	require get_theme_file_path( 'structures/404.php' );
+
+	$structures_manager->register_structure( 'Kava_Child_Structure_Archive' );
+	$structures_manager->register_structure( 'Kava_Child_Structure_404' );
+
+}
+
+function add_leaflet_scripts() {
+    // Enqueue Leaflet CSS
+    wp_enqueue_style( 'leaflet-css', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css' );
+    wp_enqueue_style( 'leaflet-markercluster-default-css', 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.Default.css' );
+    wp_enqueue_style( 'leaflet-markercluster-css', 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.css' );
+
+    // Enqueue Leaflet JavaScript
+    wp_enqueue_script( 'leaflet-js', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', array(), null, true );
+    wp_enqueue_script( 'leaflet-markercluster-js', 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/leaflet.markercluster.js', array(), null, true );
+
+    // Custom Script for Map Initialization
+    wp_enqueue_script( 'main-map-js', get_template_directory_uri() . '/main-map.js', array(), null, true );
+}
+add_action( 'wp_enqueue_scripts', 'add_leaflet_scripts' );
---
+++ main-map.js
@@ -0,0 +1,73 @@
+const svgTag = `
+<svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="17pt" height="24pt"
+    viewBox="0 0 830.000000 1280.000000" preserveAspectRatio="xMidYMid meet">
+    <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)" fill="#FF5D03"
+        stroke="none">
+        <path
+            d="M3855 12789 c-555 -44 -1043 -176 -1530 -414 -1457 -712 -2370 -2223 -2322 -3840 19 -605 152 -1155 406 -1680 109 -225 183 -353 331 -575 65 -96 856 -1369 1760 -2827 903 -1459 1646 -2653 1650 -2653 4 0 747 1194 1650 2652 904 1459 1695 2732 1760 2828 148 222 222 350 331 575 421 869 520 1869 279 2821 -244 958 -822 1795 -1640 2371 -696 491 -1551 759 -2404 752 -94 -1 -216 -5 -271 -10z m635 -1764 c440 -80 813 -271 1120 -575 769 -761 825 -1980 130 -2812 -335 -402 -817 -663 -1344 -728 -114 -14 -378 -14 -492 0 -853 105 -1550 715 -1764 1544 -141 545 -52 1136 243 1613 330 531 862 876 1497 968 130 19 481 13 610 -10z" />
+    </g>
+</svg>
+`;
+
+const svgClusterIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
+    <ellipse cx="22.4813" cy="22.5" rx="22.4813" ry="22.5" fill="#FF5D03"/>
+</svg>
+`;
+
+var points;
+
+var tagIcon = L.divIcon({
+  html: svgTag,
+  iconSize: [24, 30],
+  iconAnchor: [16, 32],
+  className: '',
+});
+
+var markers = L.markerClusterGroup({
+  spiderfyOnMaxZoom: false,
+  showCoverageOnHover: false,
+  zoomToBoundsOnClick: true,
+  spiderfyOnMaxZoom: true,
+  spiderfyOnEveryZoom: false,
+  removeOutsideVisibleBounds: true,
+  animate: true,
+  animateAddingMarkers: true,
+  chunkedLoading: false,
+  iconCreateFunction: function (cluster) {
+    return L.divIcon({
+      html: '<div><span>' + cluster.getChildCount() + '</span></div>',
+      className: 'cat-marker-cluster marker-cluster-medium',
+      iconSize: new L.Point(40, 40),
+    });
+  },
+});
+
+const map = L.map('map', {
+  zoomControl: false,
+  attributionControl: false,
+  touchZoom: false,
+}).setView([41.6938, 44.8015], 13); // tbilisi geo-position
+
+const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  maxZoom: 20,
+}).addTo(map);
+
+// Function to add markers to the map
+function addMarkers(points) {
+  points.forEach((point) => {
+    const [lat, lng] = point.geoLocation.split(', ').map(Number);
+    const marker = L.marker([lat, lng], { icon: tagIcon });
+    markers.addLayer(marker);
+  });
+
+  map.addLayer(markers);
+}
+
+fetch('https://api.catebi.ge/api/map/getcatsshort')
+  .then((response) => response.json())
+  .then((data) => {
+    points = data;
+    addMarkers(points);
+  })
+  .catch((error) => console.error('Error fetching points:', error));
