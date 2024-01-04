bfade9ef783a3d6812e741a764199a85e2f9e9ad
ks <zejafo@gmail.com>
2024-01-04 19:50:33 +0400

    fix clipboard layout; add name copying

--- clipboard/scripts/main-clipboard.js
+++ clipboard/scripts/main-clipboard.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function() {
-    var clipboard=new ClipboardJS('.copyboard-icon',{
+    var clipboard=new ClipboardJS('.copyboard-input-container',{
         target: function(trigger) {
-            return trigger.previousElementSibling;
+            return trigger.querySelector('text');
         }
     });
 
@@ -12,6 +12,26 @@ document.addEventListener('DOMContentLoaded',function() {
         tooltip.style.visibility = 'visible';
         setTimeout(function() {
             tooltip.style.visibility = 'hidden';
-        }, 1200);
+        }, 750);
+    });
+
+    var clipboardName = new ClipboardJS('.copyboard-name-container', {
+        text: function(trigger) {
+            return trigger.querySelector('div').innerText;
+        }
+    });
+
+    clipboardName.on('success', function(e) {
+        var tooltip = e.trigger.querySelector('.tooltip-name');
+        tooltip.style.visibility = 'visible';
+        tooltip.style.opacity = '1';
+        setTimeout(function() {
+            tooltip.style.visibility = 'hidden';
+            tooltip.style.opacity = '0';
+        }, 2000);
+    });
+
+    clipboardName.on('error', function(e) {
+        console.error('Error:', e);
     });
 });
