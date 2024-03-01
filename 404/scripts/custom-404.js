77483d8ab35bf5d990da9d827e44046fd6cf5ed9
ks <zejafo@gmail.com>
2024-03-01 09:21:37 +0400

    fix styles and script config

--- 404/scripts/custom-404.js
+++ 404/scripts/custom-404.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var themeImages = themeImages || { baseUrl: '../404/images/' };
+    // uncomment for local purposes
+    // var themeImages = themeImages || { baseUrl: '../404/images/' };
 
     const title = '404';
     const description = 'Something went wrong...';
@@ -10,53 +11,53 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const configs = [
         {
-            title: { fontSize: '320px', position: { left: '50%', top: '5%', transform: 'translate(-50%, -50%)'  } },
-            description: { position: { left: '50%', top: '75%', transform: 'translate(-50%, -50%)'  } },
-            imageUrl: themeImages.baseUrl + 'cat_05.png',
-            imageSize: { width: 'auto', height: '80%' },
-            imagePosition: { top: '50%', left: '50%', transform: 'translate(-50%, -50%)' },
+            title: { fontSize: '320px', position: { left: '50%', top: '15%', transform: 'translate(-50%, -50%)'  } },
+            description: { position: { left: '50%', top: '85%', transform: 'translate(-50%, -50%)'  } },
+            imageUrl: themeImages.baseUrl + 'cat_01.png',
+            imageSize: { width: 'auto', height: '50vh' },
+            imagePosition: { top: '60%', left: '50%', transform: 'translate(-50%, -50%)' },
             buttonGroup: {
-                position: { top: '85%', left: '50%', transform: 'translate(-50%, -50%)' }
+                position: { top: '100%', left: '50%', transform: 'translate(-50%, -50%)' }
             },
         },
         {
             title: { fontSize: '220px', position: { left: '10%', top: '20%', transform: 'translate(-0%, -50%)'  } },
             description: { position: { left: '10%', top: '55%', transform: 'translate(-0%, -50%)'  } },
             imageUrl: themeImages.baseUrl + 'cat_02.png',
-            imageSize: { width: 'auto', height: '80%' },
+            imageSize: { width: 'auto', height: '50vh' },
             imagePosition: { top: '40%', left: '70%', transform: 'translate(-50%, -50%)' },
             buttonGroup: {
                 position: { top: '65%', left: '10%', transform: 'translate(-00%, -50%)' }
             },
         },
         {
-            title: { fontSize: '320px', position: { left: '50%', top: '5%', transform: 'translate(-50%, -50%)'  } },
-            description: { position: { left: '50%', top: '75%', transform: 'translate(-50%, -50%)'  } },
-            imageUrl: themeImages.baseUrl + 'cat_01.png',
-            imageSize: { width: 'auto', height: '80%' },
-            imagePosition: { top: '50%', left: '50%', transform: 'translate(-50%, -50%)' },
+            title: { fontSize: '220px', position: { left: '10%', top: '20%', transform: 'translate(-0%, -50%)'  } },
+            description: { position: { left: '10%', top: '55%', transform: 'translate(-0%, -50%)'  } },
+            imageUrl: themeImages.baseUrl + 'cat_03.png',
+            imageSize: { width: 'auto', height: '50vh' },
+            imagePosition: { top: '50%', left: '70%', transform: 'translate(-50%, -50%)' },
             buttonGroup: {
-                position: { top: '85%', left: '50%', transform: 'translate(-50%, -50%)' }
+                position: { top: '65%', left: '10%', transform: 'translate(-00%, -50%)' }
             },
         },
         {
             title: { fontSize: '320px', position: { left: '50%', top: '-20%', transform: 'translate(-50%, -0%)'  } },
             description: { position: { left: '50%', top: '70%', transform: 'translate(-50%, -0%)'  } },
             imageUrl: themeImages.baseUrl + 'cat_04.png',
-            imageSize: { width: 'auto', height: '80%' },
+            imageSize: { width: 'auto', height: '50vh' },
             imagePosition: { left: '50%', top: '23%', transform: 'translate(-50%, -0%)' },
             buttonGroup: {
                 position: { left: '50%', top: '80%', transform: 'translate(-50%, -0%)' }
             },
         },
         {
-            title: { fontSize: '220px', position: { left: '10%', top: '20%', transform: 'translate(-0%, -50%)'  } },
-            description: { position: { left: '10%', top: '55%', transform: 'translate(-0%, -50%)'  } },
-            imageUrl: themeImages.baseUrl + 'cat_03.png',
-            imageSize: { width: 'auto', height: '80%' },
-            imagePosition: { top: '50%', left: '70%', transform: 'translate(-50%, -50%)' },
+            title: { fontSize: '320px', position: { left: '50%', top: '5%', transform: 'translate(-50%, -50%)'  } },
+            description: { position: { left: '50%', top: '75%', transform: 'translate(-50%, -50%)'  } },
+            imageUrl: themeImages.baseUrl + 'cat_05.png',
+            imageSize: { width: 'auto', height: '50vh' },
+            imagePosition: { top: '50%', left: '50%', transform: 'translate(-50%, -50%)' },
             buttonGroup: {
-                position: { top: '65%', left: '10%', transform: 'translate(-00%, -50%)' }
+                position: { top: '85%', left: '50%', transform: 'translate(-50%, -50%)' }
             },
         },
     ];
@@ -68,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function() {
     container.innerHTML = `
         <h1 class="title-404" style="font-size: ${config.title.fontSize}; position: absolute; top: ${config.title.position.top}; left: ${config.title.position.left}; transform: ${config.title.position.transform};">${title}</h1>
         <img src="${config.imageUrl}" style="flex: 0 1 auto; width: ${config.imageSize.width}; height: ${config.imageSize.height}; position: absolute; top: ${config.imagePosition.top}; left: ${config.imagePosition.left}; transform: ${config.imagePosition.transform}">
-        <p style="position: absolute; top: ${config.description.position.top}; left: ${config.description.position.left}; transform: ${config.title.position.transform};">${description}</p>
+        <p class="description-404" style="position: absolute; top: ${config.description.position.top}; left: ${config.description.position.left}; transform: ${config.title.position.transform};">${description}</p>
 
         <div class="button-group-404" style="position: absolute; top: ${config.buttonGroup.position.top}; left: ${config.buttonGroup.position.left}; transform: ${config.buttonGroup.position.transform}; display: flex; gap: 20px;">
         </div>
