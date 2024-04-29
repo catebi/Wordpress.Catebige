77f94c4535acd3d39325aad92bdb7268de4a6ace
ks <zejafo@gmail.com>
2024-04-29 13:00:53 +0400

    fix clipboard css and script; add woodmart 404 original page

--- clipboard/scripts/main-clipboard.js
+++ clipboard/scripts/main-clipboard.js
@@ -1,28 +1,29 @@
 document.addEventListener('DOMContentLoaded',function() {
-    var clipboard=new ClipboardJS('.copyboard-input-container',{
+    var clipboard=new ClipboardJS('.clipboard-input-container',{
         target: function(trigger) {
             return trigger.querySelector('text');
         }
     });
 
     clipboard.on('success',function(e) {
-        var tooltip = e.trigger.querySelector('.tooltip');
+        var tooltip = e.trigger.querySelector('.clipboard-tooltip');
         var tooltipText = tooltip.getAttribute('aria-label');
         tooltip.innerText = tooltipText;
         tooltip.style.visibility = 'visible';
+        tooltip.style.opacity = '1';
         setTimeout(function() {
             tooltip.style.visibility = 'hidden';
         }, 750);
     });
 
-    var clipboardName = new ClipboardJS('.copyboard-name-container', {
+    var clipboardName = new ClipboardJS('.clipboard-name-container', {
         text: function(trigger) {
-            return trigger.querySelector('div').innerText;
+            return trigger.querySelector('div.clipboard-name').innerText;
         }
     });
 
     clipboardName.on('success', function(e) {
-        var tooltip = e.trigger.querySelector('.tooltip-name');
+        var tooltip = e.trigger.querySelector('.clipboard-tooltip');
         tooltip.style.visibility = 'visible';
         tooltip.style.opacity = '1';
         setTimeout(function() {
