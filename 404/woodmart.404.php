0829421487810ec7c375f3b87120114f01b98c1b
ks <zejafo@gmail.com>
2024-04-29 13:09:05 +0400

    update 404

---
+++ 404/woodmart.404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ */
+
+get_header(); ?>
+
+<main id="main" class="site-main">
+
+<section class="error-404 not-found">
+	<div class="page-content">
+		<div id="custom-404" class="container"></div>
+	</div><!-- .page-content -->
+</section><!-- .error-404 -->
+
+</main><!-- #main -->
+
+<?php get_footer(); ?>
